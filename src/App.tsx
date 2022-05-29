553c1f6324bf4808e4cbcebdce1108ab0b18dfbe
Elmar Amanov <Elmar.amanov10@gmail.com>
2022-05-30 03:48:28 +0400

    add bootstrap import scss file and use

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+<button className="btn btn-primary">ok</button>
+      Todo App
+    </div>
+  );
+}
+
+export default App;
